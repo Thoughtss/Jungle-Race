d2190513cc836c39c538a94817f64bb6cbe625b2
Thoughtss <lk1908@hotmail.com>
2015-06-29 07:03:35 +0800

    Initial commit 2

---
+++ game/dota_addons/jungle_race/scripts/vscripts/teleporters.lua
@@ -0,0 +1,22 @@
+-- variables
+TELEPORTERS = {
+--                        entrance_name = "exit_name"
+--            example:    teleport_southeast = "southeast_exit",
+                        topright_teleporter = "btmright_teleporter",
+                        btmleft_teleporter = "topleft_teleporter" 
+--            Remember to always put a , between lines, but not on the last line
+                }
+
+function Teleport_Unit(trigger)
+    -- Find the respective exit to every entrance
+    local point = Entities:FindByName( nil, TELEPORTERS[trigger.caller:GetName()] ):GetAbsOrigin()
+    -- Teleport the unit
+    FindClearSpaceForUnit(trigger.activator, point, false)
+    -- Stop the unit or else, it might move around after being teleported.
+    trigger.activator:Stop()    
+--[[
+    print (trigger.caller:GetName())
+    print ("Teleported!")
+--]]
+    SendToConsole("dota_camera_center") -- Camera has to follow otherwise, the player might be lost.
+end
